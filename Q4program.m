e628787e52db2f0d3dbb9a7bae19e2cea86346cf
Afonso Araújo <afonso.araujo@tecnico.ulisboa.pt>
2023-12-08 17:06:02 +0000

    mirror pressão a funcionar

--- Q4program.m
+++ Q4program.m
@@ -18,8 +18,8 @@ disp('Solution...');
 u = solveSystem(Kg, fg);
 disp('System solved...');
 
-
 %%%%%%%%%%%%%%
+
 disp('Post-processing...');
 
 elementType = 'QUAD4';
@@ -28,226 +28,76 @@ Nnds = length(coordout(:, 2));
 coordx = coordout(:,2);
 coordy = coordout(:,3);
 
-[xcentroid, ycentroid] = computeCentroids(connectivityData, coordx, coordy, elementType);
-pressure = calculatePressure(connectivityData, coordx, coordy, u, elementType);
-%[fineX, fineY, fineU, fineP] = interpolateAndMask(coordx, coordy, u, pressure, xcentroid, ycentroid, fronteira, 2);
-[fineXY, fineU, fineP] = interpolateOption(coordx, coordy, u, xcentroid, ycentroid, pressure, fronteira, 1);
-[vx, vy] = calculateCentroidsVelocity(connectivityData, coordx, coordy, u, elementType);
-[Res, xint, yint, vxint, vyint] = calculateVelocityAtIntegrationPoints(connectivityData, coordx, coordy, u, 4, elementType);
+half = 0; % For half piece half = 1, whole half = 0,
 
+if half
 
+    [xcentroid, ycentroid] = computeCentroids(connectivityData, coordx, coordy, elementType);
+    pressure = calculatePressure(connectivityData, coordx, coordy, u, elementType);
+    [fineX, fineY, fineU, fineP] = interpolateAndMask(coordx, coordy, u, pressure, xcentroid, ycentroid, fronteira, 2);
+    [vx, vy] = calculateCentroidsVelocity(connectivityData, coordx, coordy, u, elementType);
+    [Res, xint, yint, vxint, vyint] = calculateVelocityAtIntegrationPoints(connectivityData, coordx, coordy, u, 4, elementType);
+    
+    % Streamlines
+    figure, subplot(2, 1, 1);
+    contourf(fineX, fineY, fineU, 20), hold on;
+    plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);
+    title('U'), xlabel('X-axis'), ylabel('Y-axis'), axis equal;hold off;
+    
+    subplot(2, 1, 2);
+    contour(fineX, fineY, fineU, 20,'LineColor','k'), hold on;
+    plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);
+    title('U'), xlabel('X-axis'), ylabel('Y-axis'), axis equal, hold off;
+    
+    % Pressure
+    figure;
+    contour(fineX, fineY, fineP, 10,'LineColor','k',"ShowText",true,"LabelFormat","%0.2f bar",'LabelSpacing', 400), hold on;
+    plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);
+    title('P'), xlabel('X-axis'), ylabel('Y-axis'), axis equal, hold off;
+    
+    % Velocity
+    figure, subplot(2, 1, 1);
+    quiver(xint, yint, vxint, vyint);
+    title('Pontos de integração'), xlabel('X-axis'), ylabel('Y-axis'), axis equal, hold off;
 
-surf(fineXY(:,1),fineXY(:,2),fineU(:))
-
-
-
-% Scatter plot for fineU
-figure;
-scatter(fineXY(:, 1), fineXY(:, 2), 50, fineU, 'filled');
-colorbar;
-title('Fine U Distribution');
-xlabel('X-axis');
-ylabel('Y-axis');
-axis equal;
-
-% Scatter plot for fineP
-figure;
-scatter(fineXY(:, 1), fineXY(:, 2), 50, fineP, 'filled');
-colorbar;
-title('Fine P Distribution');
-xlabel('X-axis');
-ylabel('Y-axis');
-axis equal;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-%%%%%%%%%%%%%% streamlines
-
-figure;
-subplot(2, 1, 1);
-contourf(fineX, fineY, fineU, 20);
-hold on;
-plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);    % Plot boundary
-title('U');
-xlabel('X-axis');
-ylabel('Y-axis');
-axis equal;
-hold off;
-
-subplot(2, 1, 2);
-contour(fineX, fineY, fineU, 20,'LineColor','k');
-hold on;
-plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);    % Plot boundary
-title('U');
-xlabel('X-axis');
-ylabel('Y-axis');
-axis equal;
-hold off;
-
-%%%%%%%%%%%%%% pressure
-
-figure;
-contour(fineX, fineY, fineP, 10,'LineColor','k',"ShowText",true,"LabelFormat","%0.2f bar",'LabelSpacing', 400);
-hold on;
-plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);    % Plot boundary
-title('P');
-xlabel('X-axis');
-ylabel('Y-axis');
-axis equal;
-hold off;
-
-%%%%%%%%%%%%%% velocity
-
-figure;
-
-subplot(2, 1, 1);
-quiver(xint, yint, vxint, vyint);
-axis equal;
-title('Pontos de integração');
-xlabel('X-axis');
-ylabel('Y-axis');
-hold off;
-
-subplot(2, 1, 2);
-quiver(xcentroid, ycentroid, vx, vy);
-axis equal;
-title('Centróides');
-xlabel('X-axis');
-ylabel('Y-axis');
-hold off;
-
-%%%%%%%%%%%%%% MIRROR pressure
-
-% Create a mirrored version of pressure along Y = 0.9 axis
-mirroredFineP = flipud(fineP);
-
-% Create meshgrid for mirrored coordinates
-fineXmir = linspace(min(coordx), max(coordx), 2 * (length(coordx) - 1) + 1);
-fineYmir = linspace(max(coordy), 2 * max(coordy), 2 * (length(coordy) - 1) + 1);
-[fineXmir, fineYmir] = meshgrid(fineXmir, fineYmir);
-
-% Combine original and mirrored data
-combinedFineX = [fineX, fineXmir];
-combinedFineY = [fineY, fineYmir];
-combinedFineP = [fineP, mirroredFineP];
-
-fill3(combinedFineX, combinedFineY,combinedFineP,combinedFineP);
-% Plot solution
-figure
-for i=1:Nels
-    edofs=[connectivityData(i,:)];
-    fill3(coordx(edofs),coordy(edofs),u(edofs),u(edofs));hold on
+    subplot(2, 1, 2);
+    quiver(xcentroid, ycentroid, vx, vy);
+    title('Centróides'), xlabel('X-axis'), ylabel('Y-axis'), axis equal, hold off;
+else
+    pressure = calculatePressure(connectivityData, coordx, coordy, u, elementType);
+    px = [coordx(:); coordx(:)];
+    py = [coordy(:); 2*max(coordy(:))-coordy(:)];
+    [pxpy, uniqueIndices] = unique([px, py], 'rows');
+    ut = [u; u];
+    ut = ut(uniqueIndices, :);  % Keep only the rows corresponding to unique points
+    px = pxpy(:,1);
+    py = pxpy(:,2);
+    B5 = flipud([B3(:,1), 2*max(coordy)-B3(:,2)]);
+    B6 = flipud([B2(:,1), 2*max(coordy)-B2(:,2)]);
+    B7 = [B1(:,1), 2*max(coordy)-B1(:,2)];
+    fronteirat = [B1;B2;B3;B5;B6;B7];
+    [xcentroid, ycentroid] = computeCentroids(connectivityData, coordx, coordy, elementType);
+    pxcentroid = [xcentroid; xcentroid];
+    pycentroid = [ycentroid; 2*max(coordy)-ycentroid];
+    pressuret = [pressure; pressure];
+    
+    [fineX, fineY, fineUt, finePt] = interpolateAndMask(px, py, ut, pressuret, pxcentroid, pycentroid, fronteirat, 2);
+    
+    % Stream lines
+    figure, subplot(2, 1, 1);
+    contourf(fineX, fineY, fineUt, 20), hold on;
+    plot([min(px), max(px)], [0.9, 0.9], 'k-', 'LineWidth', 0.5);
+    plot(fronteirat(:,1), fronteirat(:,2), 'Color', 'k', 'LineWidth', 1);
+    title('U'), xlabel('X-axis'), ylabel('Y-axis'), axis equal, axis off, hold off;
+    subplot(2, 1, 2);
+    contour(fineX, fineY, fineUt, 20,'LineColor','k'), hold on;
+    plot([min(px), max(px)], [0.9, 0.9], 'k-', 'LineWidth', 0.5);
+    plot(fronteirat(:,1), fronteirat(:,2), 'Color', 'k', 'LineWidth', 1);
+    title('U'), xlabel('X-axis'), ylabel('Y-axis'), axis equal, axis off, hold off;
+    
+    % Pressure
+    figure;
+    contourf(fineX, fineY, finePt, 10,'LineColor','k',"ShowText",true,"LabelFormat","%0.2f bar",'LabelSpacing', 400), hold on;
+    plot(fronteirat(:,1), fronteirat(:,2), 'Color', 'k', 'LineWidth', 1);
+    title('P'), xlabel('X-axis'), ylabel('Y-axis'), axis equal, hold off;
 end
-plot(coordx,coordy,'ro');
-hold off;
-
-
-
-
-% Plot contours for the entire figure
-figure;
-
-contour(combinedFineX, combinedFineY, combinedFineP, 10, 'LineColor', 'k', 'ShowText', true, 'LabelFormat', '%0.2f bar', 'LabelSpacing', 400);
-hold on;
-
-% Plot boundary
-B5 = [B1(:,1), 2*max(coordy)-B1(:,2)];
-B6 = [B3(:,1), 2*max(coordy)-B3(:,2)];
-B7 = [B2(:,1), 2*max(coordy)-B2(:,2)];
-plot(B1(:,1), B1(:,2),'k', 'LineWidth', 1),
-plot(B2(:,1), B2(:,2),'k', 'LineWidth', 1);
-plot(B3(:,1), B3(:,2),'k', 'LineWidth', 1);
-plot(B5(:,1), B5(:,2),'k', 'LineWidth', 1);
-plot(B6(:,1), B6(:,2),'k', 'LineWidth', 1);
-plot(B7(:,1), B7(:,2),'k', 'LineWidth', 1);
-
-title('P');
-xlabel('X-axis');
-ylabel('Y-axis');
-axis equal;
-legend('Combined Boundary');
-hold off;
-
-
-
-
-
-
-
-%%%%%%%%% Mirror stream lines
-
-figure;
-contourf(fineX, fineY, fineU, 20);
-hold on;
-plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);    % Plot boundary
-title('U');
-xlabel('X-axis');
-ylabel('Y-axis');
-axis equal;
-hold off;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% % Original pressure plot
-% figure;
-% 
-% contour(fineX, fineY, fineP, 10,'LineColor','k',"ShowText",true,"LabelFormat","%0.2f bar",'LabelSpacing', 400);
-% hold on;
-% plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);    % Plot boundary
-% title('Original Pressure');
-% xlabel('X-axis');
-% ylabel('Y-axis');
-% axis equal;
-% hold off;
-
-% % Create a mirrored version of pressure along y = 3.8 axis
-% mirroredPressure = flipud(fineP);
-% 
-% % Plot mirrored pressure along y = 3.8 axis
-% figure;
-% contour(fineX, fineY, mirroredPressure, 10,'LineColor','k',"ShowText",true,"LabelFormat","%0.2f bar",'LabelSpacing', 400);
-% hold on;
-% plot(fronteira(:,1), fronteira(:,2), 'Color', 'k', 'LineWidth', 1);    % Plot boundary
-% title('Mirrored Pressure along y = 3.8 axis');
-% xlabel('X-axis');
-% ylabel('Y-axis');
-% axis equal;
-% hold off;
-
